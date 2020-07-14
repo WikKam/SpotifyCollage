5b027ff9740832b39c8e01a58a3c90c31fd8178d
WikKam <kamykikamienie@gmail.com>
2020-07-14 15:27:28 +0200

    init

---
+++ src/main/java/pl/wikkam/spotifychart/SpotifychartApplication.java
@@ -0,0 +1,13 @@
+package pl.wikkam.spotifychart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifychartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifychartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wikkam/spotifychart/SpotifychartApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wikkam.spotifychart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifychartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
