00165241935222e8dc58ef61873c43b5a8f1b361
WikKam <kamykikamienie@gmail.com>
2020-07-23 20:37:50 +0200

    d

--- src/main/java/pl/wikkam/spotifychart/config/SpotifyConfig.java
+++ src/main/java/pl/wikkam/spotifychart/config/SpotifyConfig.java
@@ -1,17 +1,28 @@
 package pl.wikkam.spotifychart.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
-@EnableOAuth2Sso
-public class SpotifyConfig extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+public class SpotifyConfig extends ResourceServerConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().antMatchers("/login").authenticated();
+    public void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
+
+    @Primary
+    @Bean
+    public UserInfoTokenServices tokenService() {
+        final UserInfoTokenServices tokenService = new UserInfoTokenServices("/user", "");
+        return tokenService;
     }
 
 }
